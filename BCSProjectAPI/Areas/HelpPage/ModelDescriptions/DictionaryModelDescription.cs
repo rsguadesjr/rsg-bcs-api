fea1266b2d2bcc240a28081d0aa29dcaa3786b1f
Desiree Ann <Desiree Ann@EDDIE-C-DONS>
2018-07-17 12:44:48 +0800

    Add project files.

---
+++ BCSProjectAPI/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace BCSProjectAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ BCSProjectAPI/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace BCSProjectAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
