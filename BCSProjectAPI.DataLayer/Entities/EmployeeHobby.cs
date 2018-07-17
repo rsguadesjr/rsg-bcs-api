ed3a277125fc274edf6f12c8943d189ce797c7fb
Raymond <rsguadesjr@gmail.com>
2018-07-18 00:19:25 +0800

    Initial Project Structure

---
+++ BCSProjectAPI.DataLayer/Entities/EmployeeHobby.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BCSProjectAPI.DataLayer.Entities
+{
+    public class EmployeeHobby
+    {
+        public int Id { get; set; }
+        public int HobbyId { get; set;}
+        public int EmployeeID { get; set; }
+    }
+}
